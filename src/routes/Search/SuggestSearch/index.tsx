34b5242790c5886e5c197bc866f850e1df601118
bugeon <bearbu@naver.com>
2022-05-20 17:53:52 +0900

    Style: Set Search Design same with 'https://clinicaltrialskorea.com/'
    Co-authored-by: 권은서 kwonenseo@gmail.com
    Co-authored-by: zellytozelly zellytozelly@gmail.com
    Co-authored-by: Yeonghyeon Kim kyhyun@users.noreply.github.com
    Co-authored-by: Sangwon Lee bisari31@gmail.com

---
+++ src/routes/Search/SuggestSearch/index.tsx
@@ -0,0 +1,3 @@
+const SuggestSearch = () => {}
+
+export default SuggestSearch
--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,5 +1,39 @@
+import { ChangeEvent, useState } from 'react'
+
+import { SearchIcon } from 'assets'
+import styles from './Search.module.scss'
+
 const Search = () => {
-  return <h1>휴먼스케이프</h1>
+  const [searchText, setSearchText] = useState('')
+
+  const handleChangeSearchText = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.currentTarget.value)
+  }
+
+  return (
+    <div className={styles.searchContainer}>
+      <p className={styles.description}>
+        국내 모든 임상시험 검색하고
+        <br /> 온라인으로 참여하기
+      </p>
+
+      <div className={styles.searchInputWarrper}>
+        <SearchIcon />
+        <input
+          className={styles.searchInput}
+          value={searchText}
+          onChange={handleChangeSearchText}
+          placeholder='질환명을 입력해 주세요.'
+        />
+        <button type='button' className={styles.searchButton}>
+          검색
+        </button>
+      </div>
+      <ul className={styles.dropdown}>
+        <span>추천 검색어</span>
+      </ul>
+    </div>
+  )
 }
 
 export default Search
