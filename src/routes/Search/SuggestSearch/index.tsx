ce5f48e0694f834112b22ac699cf53e5acff1eca
bugeon <bearbu@naver.com>
2022-05-22 04:22:32 +0900

    Refactor: change scss order and adjust dropdown maxheight for iphoneSE

--- src/routes/Search/SuggestSearch/index.tsx
+++ src/routes/Search/SuggestSearch/index.tsx
@@ -11,9 +11,10 @@ import styles from '../Search.module.scss'
 import { SearchIcon } from 'assets'
 import HighlightText from './HighlightText'
 
+const SEARCH_URL = 'https://clinicaltrialskorea.com/studies?condition='
+
 const SuggestSearch = ({ query }: { query: string }) => {
   const index = useAppSelector(getItemIndex)
-  const searchUrl = 'https://clinicaltrialskorea.com/studies?condition='
   const pageEndPointRef = useRef<HTMLDivElement>(null)
 
   const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } = useInfiniteQuery(
@@ -49,7 +50,7 @@ const SuggestSearch = ({ query }: { query: string }) => {
         page.items.map((item: IItem, i: number) => (
           <li key={item.sickCd} className={cx({ [styles.isFocus]: index === i })}>
             <SearchIcon />
-            <a href={searchUrl + item.sickNm}>
+            <a href={SEARCH_URL + item.sickNm}>
               <HighlightText query={query} text={item.sickNm} />
             </a>
           </li>
--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,14 +1,13 @@
 import { KeyboardEvent, ChangeEvent, Suspense, useRef, useState } from 'react'
 import { useMount } from 'react-use'
 import { ErrorBoundary } from 'react-error-boundary'
-
 import { useAppDispatch, useAppSelector, useQueryDebounce } from 'hooks'
-import SuggestSearch from './SuggestSearch'
-import Header from './Header/Header'
-import { getItemIndex, setItemIndex } from 'store/searchIndex'
 
 import styles from './Search.module.scss'
 import { SearchIcon } from 'assets'
+
+import { getItemIndex, setItemIndex } from 'store/searchIndex'
+import SuggestSearch from './SuggestSearch'
 
 const Search = () => {
   const [searchText, setSearchText] = useState('')
@@ -17,10 +16,6 @@ const Search = () => {
   const handleChangeSearchText = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.currentTarget.value)
   }
-
-  useMount(() => {
-    inputRef.current?.focus()
-  })
 
   const keyIndexRef = useRef<HTMLUListElement>(null)
   const index = useAppSelector(getItemIndex)
@@ -35,20 +30,24 @@ const Search = () => {
     if (e.key === 'ArrowDown') {
       dispatch(setItemIndex(index + 1))
       currentValue = keyIndexRef.current?.childNodes[index + 2]?.textContent
-      // if (keyIndexRef.current?.childElementCount === index + 1) dispatch(setItemIndex(0))
     }
+
     if (e.key === 'ArrowUp') {
       dispatch(setItemIndex(index - 1))
       currentValue = keyIndexRef.current?.childNodes[index]?.textContent
       if (index < 0) dispatch(setItemIndex(-1))
     }
+
     if (currentValue === undefined || currentValue === null) currentValue = searchText
     setSearchText(currentValue)
   }
 
+  useMount(() => {
+    inputRef.current?.focus()
+  })
+
   return (
     <div className={styles.searchContainer}>
-      <Header />
       <p className={styles.description}>
         국내 모든 임상시험 검색하고
         <br /> 온라인으로 참여하기
